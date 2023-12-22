77af959c207822a45d811c373748800436a5bbe2
MiniDigger | Martin <admin@benndorf.dev>
2023-12-22 10:32:50 +0100

    feat: setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,28 @@ tasks.test {
     }
 }
 
+tasks.jar {
+    manifest {
+        attributes("Implementation-Version" to project.version)
+    }
+}
+
 publishing {
+    repositories {
+        maven("https://repo.papermc.io/repository/maven-snapshots/") {
+            credentials(PasswordCredentials::class)
+            name = "paper"
+        }
+    }
+
     publications.create<MavenPublication>("maven") {
         artifactId = "restamp"
         from(components["java"])
+    }
+}
+
+tasks.register("printVersion") {
+    doFirst {
+        println(version)
     }
 }
